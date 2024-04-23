541e03708f1e10a6eba4d51c82c89c69ebc67b1e
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-04-22 22:16:40 -0700

    Add TailwindCSS

--- my-app/src/routes/+page.svelte
+++
@@ -1,2 +0,0 @@
-<h1>Concert Tracker</h1>
-<p>Displays all the concerts around the user's specified area including artist name, concert date, etc.</p>
---
+++ server/index.js
@@ -0,0 +1,2 @@
+// TODO: Start by Creating an app on ticket master api so I can use their api
+// TODO: ask the user for  input about a city of their choice and print to the terminal
