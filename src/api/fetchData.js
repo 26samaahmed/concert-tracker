5b919904122cd95f0106cf16d3e00a600e334587
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-07-18 00:13:10 -0700

    Convert Date and Time

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,7 +1,8 @@
 export async function fetchData(location) {
   let eventsArray = [];
   try {
-    const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&classificationName=music&city=${location}&apikey=${key}`);
+    // fetch data from Ticketmaster API using the location provided by the user
+    const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&classificationName=music&city=${location}&apikey=${apiKey}`);
 
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
@@ -10,6 +11,7 @@ export async function fetchData(location) {
     const json = await response.json();
     //console.log(json);
     //console.log(json.pages.size)
+    // Parse the JSON data and store it in an array of objects with each event's details
     for (var i = 0; i < 100; i++) {
       eventsArray.push({
         image: json._embedded.events[i].images[0].url,
@@ -33,5 +35,37 @@ export async function fetchData(location) {
   }
 }
 
+// Converts a date from 'YYYY-MM-DD' to for this format 'Sat Sep 14 2024'
+export function convertDate(stringDate) {
+  let newDate = new Date(stringDate);
+  return newDate.toDateString();
+}
 
-// TODO: Add a function that converts military time to standard time and also retrieves the day of the week
+// Converts time from '18:30:00' to this format '7:30 PM'
+export function convertTime(time) {
+  // Split the string to access hour and minutes individually
+  time = time.split(':');
+  let hour = Number(time[0]);
+  let minute = Number(time[1]);
+
+  let newHour;
+  let am_or_pm = "AM"
+
+  // if hours are abover 12, subtract from 12
+  if (hour > 0 && hour < 12) {
+    newHour = "" + hour;
+  } else if (hour > 12) {
+    newHour = "" + (hour - 12);
+    am_or_pm = "PM"
+  } else {
+    newHour = "12";
+  }
+
+  // if minutes are below 10, concatenate a 0 before the one digit
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
+
+  newHour += ":" + minute + " " + am_or_pm;
+  return newHour;
+}
